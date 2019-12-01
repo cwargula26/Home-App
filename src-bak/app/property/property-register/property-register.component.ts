069728a0f9df744e479fc254477e8a7600ee6b84
Chris Wargula <chris.wargula@gmail.com>
2019-12-01 16:03:25 -0500

    commit

---
+++ src-bak/app/property/property-register/property-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-register',
+  templateUrl: './property-register.component.html',
+  styleUrls: ['./property-register.component.css']
+})
+export class PropertyRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

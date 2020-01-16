b043b64166be7a63b9fad44e66cd0b1de7e5f8af
Chris Wargula <chris.wargula@gmail.com>
2020-01-16 06:44:49 -0500

    Set up theme for site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,29 @@
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AuthInterceptor, AuthService, FakeBackendInterceptor } from '@services/*';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ChartsModule } from './pages/charts';
-import { ComponentsModule } from './pages/components';
-import { DashboardModule } from './pages/dashboard';
-import { Dashboard2Module } from './pages/dashboard2';
-import { FormsModule } from './pages/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { BlankTemplateComponent } from "./template/blank-template.component";
+import { LeftNavTemplateComponent } from "./template/left-nav-template.component";
+import { AppRoutingModule, routes } from "./app.routing";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { NavigationComponent } from "./shared/navigation/navigation.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    BlankTemplateComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    LeftNavTemplateComponent,
+    NavigationComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule,
-    DashboardModule,
-    Dashboard2Module,
-    FormsModule,
-    ChartsModule,
-    HttpClientModule,
+    RouterModule.forRoot(routes, { useHash: true })
   ],
-  providers: [
-    AuthService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: FakeBackendInterceptor,
-      multi: true,
-    },
-  ],
-  bootstrap: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
